e5ea56266712953e6d6951311cdd34cf101937da
Connor Brookes <vbrookesy@hotmail.co.uk>
2019-10-23 10:50:28 +0100

    fin first if

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 view.roll.addEventListener('click',()=> Gamepad.turn(), false);
 let diceAnimate = dice =>{
-    if(dice.position === 'idle');
+    if(dice.position === 'idle'){
+        dice.o.classList.remove('idle')
+    }
 
 }
